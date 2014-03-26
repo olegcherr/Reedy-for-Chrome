da152c8aff4da486f4c3671117001cd2751a98b9
Oleg Cherr <olegcherr@gmail.com>
2014-03-26 22:28:07 +0400

    The extensions is ready to work right after its installation. No page refresh required

--- js/background.js
+++ js/background.js
@@ -2,6 +2,40 @@
 
 (function() {
 	
+	/*function checkTabsAliveness() {
+		function onResult(res) {
+			var index = tabIds.indexOf(res);
+			if (index > -1) {
+				tabIds.splice(index, 1);
+			}
+		}
+		
+		function onComplete() {
+			for (var i = 0, id; i < tabIds.length; i++) {
+				id = tabIds[i];
+				chrome.tabs.executeScript(id, {file: 'js/content/main.js'});
+				chrome.tabs.executeScript(id, {file: 'js/content/Parser.js'});
+				chrome.tabs.executeScript(id, {file: 'js/content/Reader.js'});
+				chrome.tabs.executeScript(id, {file: 'js/content/unittests.js'});
+			}
+		}
+		
+		var tabIds = [];
+		
+		chrome.tabs.query({}, function(tabs) {
+			if (tabs.length) {
+				for (var i = 0, tid; i < tabs.length; i++) {
+					tid = tabs[i].id;
+					tabIds.push(tid);
+					chrome.tabs.executeScript(tid, {code: tid+';'}, onResult);
+				}
+				
+				setTimeout(onComplete, 500);
+			}
+		});
+	}*/
+	
+	
 	function settingsGet(key, callback) {
 		chrome.storage.sync.get(defaults, function(items) {
 			callback(key != null ? items[key] : items);
@@ -65,6 +99,21 @@
 		if (data.menuItemId == 'fastReaderMenu') {
 			chrome.tabs.executeScript(null, {
 				code: 'window.fastReader && window.fastReader.start();'
+			});
+		}
+	});
+	
+	
+	chrome.runtime.onInstalled.addListener(function(details) {
+		if (details.reason === "install") {
+			chrome.tabs.query({}, function(tabs) {
+				for (var i = 0, tid; i < tabs.length; i++) {
+					tid = tabs[i].id;
+					chrome.tabs.executeScript(tid, {file: 'js/content/main.js'});
+					chrome.tabs.executeScript(tid, {file: 'js/content/Parser.js'});
+					chrome.tabs.executeScript(tid, {file: 'js/content/Reader.js'});
+					chrome.tabs.executeScript(tid, {file: 'js/content/unittests.js'});
+				}
 			});
 		}
 	});
