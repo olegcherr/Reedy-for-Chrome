838b8d6b4979a2f61050bc0a255801f71f56213e
Christoph Zwerschke <cito@online.de>
2018-07-09 15:01:41 +0200

    Use hardcoded list of fonts
    Since Firefox doesn't support the fontSettings API yet,
    we use a hard coded list of web safe CSS fonts.

--- js/popup/popup.js
+++ js/popup/popup.js
@@ -1,69 +1,69 @@
 
 
 chrome.runtime.getBackgroundPage(function(bgWindow) {
-	
+
 	function querySelector(selector) {
 		return document.querySelector(selector);
 	}
-	
+
 	function querySelectorAll(selector) {
 		return document.querySelectorAll(selector);
 	}
-	
+
 	function getTextSelection(callback) {
 		app.sendMessageToSelectedTab({type: 'getSelection'}, function(sel) {
 			callback(sel || '');
 		});
 	}
-	
-	
+
+
 	function onExternalLinkClick(e) {
 		app.event('External link', e.target.href);
 		window.open(e.target.href);
 	}
-	
+
 	function onSwitchBtnClick(e) {
 		var viewName = e.target.getAttribute('switch-to');
 		switchToView(viewName);
 	}
-	
-	
+
+
 	function onCheckbox(value, $checkbox, api) {
 		app.setSettings($checkbox.name, value);
 	}
-	
+
 	function onRange(value, $input, api) {
 		app.setSettings($input.name, value);
 	}
-	
-	
+
+
 	function onStartReadingClick() {
 		app.event('Reader', 'Open', 'Popup');
 		startReading();
 	}
-	
+
 	function onStartSelectorClick() {
 		app.event('Content selector', 'Start', 'Popup');
 		startSelector();
 	}
-	
+
 	function onCloseReaderClick() {
 		app.event('Reader', 'Close', 'Popup');
 		closeReader();
 	}
-	
+
 	function onOfflineBtnClick() {
 		app.event('Offline', 'Open');
 		window.open(app.offlineUrl);
 	}
-	
-	
+
+
 	function onShortcutInputKeydown(e) {
 		app.stopEvent(e);
 		newShortcut = app.eventToShortcutData(e);
 		$iShortcut.value = app.shortcutDataToString(newShortcut, true);
 	}
-	
+
 	function onSaveShortcutBtn() {
 		if (newShortcut) {
 			if (app.checkShortcut(newShortcut)) {
@@ -79,22 +79,22 @@ chrome.runtime.getBackgroundPage(function(bgWindow) {
 				return;
 			}
 		}
-		
+
 		switchToView('main');
 	}
-	
+
 	function onCancelShortcutBtn() {
 		$iShortcut.value = app.shortcutDataToString(runShortcut, true);
 		switchToView('main');
 	}
-	
+
 	function updateShortcutElems(data) {
 		app.each(querySelectorAll('.j-shortcut'), function($elem) {
 			$elem.innerHTML = app.shortcutDataToString(data);
 		});
 	}
-	
-	
+
+
 	function onKeyDown(e) {
 		if (runShortcut && app.checkEventForShortcut(e, runShortcut)) {
 			app.stopEvent(e);
@@ -110,13 +110,13 @@ chrome.runtime.getBackgroundPage(function(bgWindow) {
 			});
 		}
 	}
-	
-	
+
+
 	function startReading() {
 		app.sendMessageToSelectedTab({type: 'startReading'});
 		window.close();
 	}
-	
+
 	function startSelector() {
 		app.isSystemTab(function(isSystem) {
 			if (isSystem) {
@@ -128,109 +128,121 @@ chrome.runtime.getBackgroundPage(function(bgWindow) {
 			}
 		});
 	}
-	
+
 	function closeReader() {
 		app.sendMessageToSelectedTab({type: 'closeReader'});
 		window.close();
 	}
-	
+
 	function switchToView(name) {
 		app.each($views, function($view) {
 			$view.setAttribute('active', $view.getAttribute('view-name') === name);
 		});
 		$body.setAttribute('active-view', name);
-		
+
 		if (name === 'shortcut')
 			$iShortcut.focus();
-		
+
 		app.event('Popup', 'Switch to', name);
 	}
-	
+
 	function initControls(settings) {
 		app.each(querySelectorAll('.j-checkbox'), function($elem) {
 			$elem.checked = settings[$elem.name];
 			new app.Checkbox($elem, onCheckbox);
 		});
-		
+
 		app.each(querySelectorAll('.j-range'), function($elem) {
 			$elem.value = settings[$elem.name];
 			new app.Range($elem, +$elem.getAttribute('min-value'), +$elem.getAttribute('max-value'), onRange);
 		});
-		
+
 		runShortcut = settings.runShortcut;
 		$iShortcut.value = app.shortcutDataToString(runShortcut, true);
 		updateShortcutElems(runShortcut);
-		
-		
-		chrome.fontSettings.getFontList(function(fontList) {
-			initThemeControls(settings, fontList);
-		});
+
+		// Since Firefox doesn't support the fontSettings API yet,
+		// we use a hard coded list of web safe CSS fonts:
+		const fontList = [
+			'Andale Mono', 'Arial', 'Arial Black', 'Arial Narrow',
+			'Arial Rounded MT Bold', 'Avant Garde', 'Baskerville', 'Big Caslon',
+			'Bodoni MT', 'Book Antiqua', 'Brush Script MT', 'Calibri',
+			'Calisto MT', 'Cambria', 'Candara', 'Century Gothic', 'Consolas',
+			'Copperplate', 'Courier New', 'Didot', 'Franklin Gothic Medium',
+			'Futura', 'Garamond', 'Geneva', 'Georgia', 'Gill Sans',
+			'Goudy Old Style', 'Helvetica', 'Hoefler Text', 'Impact',
+			'Lucida Bright', 'Lucida Console', 'Lucida Grande',
+			'Lucida Sans Typewriter', 'Monaco', 'Optima', 'Palatino', 'Papyrus',
+			'Perpetua', 'Rockwell Extra Bold', 'Rockwell', 'Segoe UI', 'Tahoma',
+			'Times New Roman', 'Trebuchet MS', 'Verdana'];
+
+		initThemeControls(settings, fontList);
 	}
-	
+
 	function initThemeControls(settings, fontList) {
 		function updateThemeControls() {
 			app.each(colorPickerApis, function(api) {
 				api.setValue(theme[api.$input.name]);
 			});
-			
+
 			app.each(themeCheckboxApis, function(api) {
 				api.setState(theme[api.$checkbox.name]);
 			});
-			
+
 			$iFontFamily.value = DEFAULT_FONT_VALUE;
 			if (theme.font_family != null)
 				app.each(fontList, function(item) {
-					if (theme.font_family === item.fontId) {
-						$iFontFamily.value = item.fontId;
+					if (theme.font_family === item) {
+						$iFontFamily.value = item;
 						return false;
 					}
 				});
 		}
-		
-		
+
+
 		var DEFAULT_FONT_VALUE = "-",
 			themeName = settings.darkTheme ? "dark" : "light",
 			theme = settings.theme[themeName],
 			colorPickerApis = [],
 			themeCheckboxApis = [],
 			$iFontFamily = querySelector(".j-iFont");
-		
-		
-		// Building fint list
+
+
+		// Building font list
 		(function(html, i) {
 			function append(value, text) {
 				html += "<option value='"+value+"'>"+text+"</option>";
 			}
-			
+
 			append(DEFAULT_FONT_VALUE, app.t("defaultFont"));
-			
+
 			for (i = 0; i < fontList.length; i++) {
-				append(fontList[i].fontId, fontList[i].fontId);
+				append(fontList[i], fontList[i]);
 			}
-			
+
 			$iFontFamily.innerHTML = html;
 		})("");
-		
-		
+
+
 		app.each(querySelectorAll(".j-iColorPicker"), function($elem) {
 			colorPickerApis.push(new app.ColorPicker($elem, function(value, $input) {
 				app.setThemeSettings(themeName, $input.name, value);
 				theme[$input.name] = value;
 			}));
 		});
-		
+
 		app.each(querySelectorAll(".j-iThemeCheckbox"), function($elem) {
 			themeCheckboxApis.push(new app.Checkbox($elem, function(value, $checkbox) {
 				app.setThemeSettings(themeName, $checkbox.name, value);
 				theme[$checkbox.name] = value;
 			}));
 		});
-		
+
 		app.on($iFontFamily, "change", function() {
 			app.setThemeSettings(themeName, $iFontFamily.name, $iFontFamily.value === DEFAULT_FONT_VALUE ? null : $iFontFamily.value);
 			theme.font_family = $iFontFamily.value;
 		});
-		
+
 		app.on(querySelector(".j-resetThemeBtn"), "click", function() {
 			app.resetThemeSettings(themeName, function() {
 				app.getSettings("theme."+themeName, function(t) {
@@ -240,37 +252,37 @@ chrome.runtime.getBackgroundPage(function(bgWindow) {
 			});
 			app.event("Popup", "Reset theme");
 		});
-		
-		
+
+
 		updateThemeControls();
 	}
-	
-	
+
+
 	var app = bgWindow.reedy,
 		runShortcut, newShortcut,
-		
+
 		$body = querySelector('body'),
 		$startReadingBtn = querySelector('.j-startReadingBtn'),
 		$startSelectorBtn = querySelector('.j-startContentSelectorBtn'),
 		$closeReaderBtn = querySelector('.j-closeReaderBtn'),
 		$views = querySelectorAll('[view-name]'),
-		
+
 		$iShortcut = querySelector('.j-iShortcut'),
 		$saveShotrcutBtn = querySelector('.j-saveShortcutBtn'),
 		$cancelShotrcutBtn = querySelector('.j-cancelShotrcutBtn'),
-		
+
 		tabs = app.Tabs(
 			"settings",
 			querySelector('.j-tabs'),
 			querySelectorAll('.j-tab'),
 			querySelectorAll('.j-tabContent')
 		);
-	
-	
+
+
 	chrome.runtime.connect({name: "Popup"});
-	
+
 	app.localizeElements(document);
-	
+
 	/**
 	 * Preparing buttons
 	 * `getTextSelection` - is a pretty diffcult method, so we check for the reader state at first
@@ -292,28 +304,28 @@ chrome.runtime.getBackgroundPage(function(bgWindow) {
 			});
 		}
 	});
-	
+
 	app.getSettings(null, initControls);
-	
-	
+
+
 	app.on(document, "keydown", onKeyDown);
-	
+
 	app.on($startReadingBtn, "click", onStartReadingClick);
 	app.on($startSelectorBtn, "click", onStartSelectorClick);
 	app.on($closeReaderBtn, "click", onCloseReaderClick);
-	
+
 	app.on(querySelector('.j-offlineBtn'), "click", onOfflineBtnClick);
-	
+
 	app.each(querySelectorAll('a[href^=http]'), function($elem) {
 		app.on($elem, 'click', onExternalLinkClick);
 	});
 	app.each(querySelectorAll('[switch-to]'), function($elem) {
 		app.on($elem, 'click', onSwitchBtnClick);
 	});
-	
+
 	app.on($iShortcut, "keydown", onShortcutInputKeydown);
 	app.on($saveShotrcutBtn, "click", onSaveShortcutBtn);
 	app.on($cancelShotrcutBtn, "click", onCancelShortcutBtn);
-	
-	
+
+
 });
